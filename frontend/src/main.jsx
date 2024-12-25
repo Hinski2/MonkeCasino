e615b380643f61dc93c57338240ebd8bf127f9be
Hinski2 <kubailinski2@gmail.com>
2024-12-26 00:51:43 +0100

    add greeting page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './styles/index.module.css'; 
+import Greeting from './pages/Greeting'
+// import Casino from './pages/Casino/Casino'
+import Login from './pages/Login'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Greeting/>,
+    },
+    {
+        path: '/login',
+        element: <Login/>
+    }
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+);
